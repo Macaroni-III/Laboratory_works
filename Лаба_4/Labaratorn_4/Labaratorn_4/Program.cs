33e6d0fface78fe90563a2eb54cedcfd588a9f3b
Macaroni-III <74963825+Macaroni-III@users.noreply.github.com>
2021-03-28 23:43:50 +0700

    Add files via upload

---
+++ Лаба_4/Labaratorn_4/Labaratorn_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Labaratorn_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    class search 
+    {
+        
+    }
+
+}
