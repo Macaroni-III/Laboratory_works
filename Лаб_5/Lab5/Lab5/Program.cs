c5086d25e147e486a3e94b4421bea64664457e3e
Macaroni-III <74963825+Macaroni-III@users.noreply.github.com>
2021-04-04 22:06:30 +0700

    Add files via upload

---
+++ Лаб_5/Lab5/Lab5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lab5
+{
+
+    /* class vocabulary
+	{
+		//miss - mistakes 
+		string[] misPrivet = new string[] { "превет","привит","првет", "првт", "ghbdtn", "пирвет" } ;
+		string[] 
+	}*/
+	class Program
+	{	
+		static void Main(string[] args)
+		{
+			char a = 'A';
+			char b = '\x5B';
+            char c = '\u0433';
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+			Console.WriteLine(c);
+		}
+	}
+}
